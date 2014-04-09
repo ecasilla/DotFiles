a2f500c6ad4fb79281c6e64f17bbc54a9d3c031e
%%GITNAME%% <%%GITEMAIL%%>
2014-04-09 17:00:02 -0400

    mongorc

---
+++ mongorc.js
@@ -0,0 +1,29 @@
+EDITOR="/usr/bin/vim"
+
+prompt = function () {
+  return (new Date())+">";
+}
+
+var compliment = [
+"smart" 
+"cool" 
+"funny" 
+"brave" 
+"kind"
+"nice" 
+"pretty" 
+"sweet" 
+"trustworthy" 
+"reliable" 
+"pleasant" 
+"interesting" 
+"polite" 
+"courteous" 
+"friendly" 
+"charming" 
+"understanding" 
+"considerate" 
+"enthusiastic" 
+"sympathetic"
+"thoughtful"
+]
