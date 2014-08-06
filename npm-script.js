2c9ee2bdc1c2b92b638b69315bbd251ecd52fc72
Ernie <ecasilla@icloud.com>
2014-08-06 13:54:28 -0400

    finishing up the npm script

--- npm-script.js
+++ npm-script.js
@@ -1,8 +1,10 @@
 var fs = require('fs'),
-npm = require('npm');
+    path = require('path'),
+    npm = require('npm');
 
 function npmGlobalInstaller(file){
   var modules = [];
+  var global = '/usr/local/lib';
   fs.readFile(file,'utf-8',function(err,data){
     if(err) console.log(err);
     modulesStr = data.split('\n'); 
@@ -10,25 +12,21 @@ function npmGlobalInstaller(file){
       if(modulesStr[i]){
         modules.push(modulesStr[i])
       }
+  });
+  npm.load({
+    loaded: false
+  }, function (err) {
+    if(err){console.log(err)}
+    npm.commands.install(global,modules, function (err, data) {
+      if(err){console.log(err + " install Err ")}
+      console.log(data)
     });
-      npm.load({
-        loaded: false
-      }, function (err) {
-        if(err){console.log(err)}
-        var global = '/usr/local/lib'
-        npm.commands.install(global,modules, function (err, data) {
-          if(err){console.log(err + " install Err ")}
-          console.log(data)
-        });
-        npm.on("log", function (message) {
-          // log the progress of the installation
-          console.log(message);
-        });
-      })
-
-  
+    npm.on("log", function (message) {
+      // log the progress of the installation
+      console.log(message);
+    });
+  })
 }
 
 npmGlobalInstaller('Npmfile')
-
 //module.exports = npmGlobalInstaller
