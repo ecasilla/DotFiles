80bf8d2deab2102ea8202c99aaeb47fcefcce011
Casilla <502190@nyc-e80964m.la.frd.directv.com>
2014-08-27 12:48:14 -0400

    added go complete.sh for auto comp in go projects

---
+++ gocomplete.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Bash completion script for the go commandline tool.  Presently it activates
+# to show packages under $GOPATH on these subcommands:
+# - go install
+# - go build
+# - go test
+#
+# Install by sourcing this file from your .bash_profile, or copy it in.
+#
+# Author: Rob Figueiredo (robfig@gmail.com)
+
+function _gocomplete() {
+# Expect the second word to be the go sub-command.
+subcmd="${COMP_WORDS[1]}"
+case $subcmd in
+  buildld|install|test)
+    # List directories with ls -d $input*/ , while ignoring stderr output
+    COMPREPLY=($(cd $GOPATH/src && ls -d $2*/ 2>/dev/srcnull))
+    return 0
+    ;;
+esac
+return 1
+}
+
+# -o default falls back to default matching if we found nothing
+# -o nospace strips trailing space from the completion
+complete -o default -o nospace -F _gocomplete go
