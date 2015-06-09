52cc2b81978c56ad5c44cdd294188e80e8925cd2
Ernie Casilla <ecasilla@icloud.com>
2015-06-08 20:50:57 -0400

    simplified the npm script and testing live log

--- git_loglive.sh
+++ git_loglive.sh
@@ -3,6 +3,8 @@
 while :
 do
   clear
-  git --no-pager log --reverse --graph --pretty=oneline --abbrev-commit --decorate --all $*
+  #git --no-pager log --graph --pretty=oneline --abbrev-commit --decorate --all $*
+  git  log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all $*
   sleep 1
 done
+
