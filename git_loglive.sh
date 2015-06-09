8642151098e6671e1ef49f9156eb3c08ea27e701
Ernie Casilla <ecasilla@icloud.com>
2015-06-08 20:35:55 -0400

    testing printing in reverse

--- git_loglive.sh
+++ git_loglive.sh
@@ -3,6 +3,6 @@
 while :
 do
   clear
-  git --no-pager log --graph --pretty=oneline --abbrev-commit --decorate --all $*
+  git --no-pager log --reverse --graph --pretty=oneline --abbrev-commit --decorate --all $*
   sleep 1
 done
